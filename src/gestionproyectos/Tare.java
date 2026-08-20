eb05790cd6a373cc634dd0361eb8575be24b75a8
marhan <marifernandapiedamartinex59@gmail.com>
2026-08-20 02:36:14 -0500

    feat:agraga consturctor de Tare,java

---
+++ src/gestionproyectos/Tare.java
@@ -0,0 +1,15 @@
+private class impresora (){
+    private int numero_botones;
+    private  string color;
+    private  float marca;
+    private string  tamano;
+    private  strig modelo;
+    private  int peso;
+ 
+ private impresora(int numero_botones, string color){
+    this.numero_botones=numero_botones
+    this.color=color 
+    
+ }
+
+}
