f5e028d56721c3a6f77e173cc12148349da247db
marhan <marifernandapiedamartinex59@gmail.com>
2026-08-20 07:49:42 -0500

    feta :agragr calse gueeri hernecia tarea

---
+++ src/gestionproyectos/guerrero.java
@@ -0,0 +1,12 @@
+private class guerrero {
+    string nombre;
+    public void  combate(){
+        system.ou.printl("guerro esta e combate")
+
+    }
+    
+
+}
+private class juan estends guerrero
+private void luchar 
+syrema out pirnt("esta luchando ")
