a94ecaa5656bb964b1cea3e866b0d3fdab78e23c
marhan <marifernandapiedamartinex59@gmail.com>
2026-08-24 12:52:53 -0500

    feat: estruteura basica de arquro

---
+++ src/gestionproyectos/arquero.java
@@ -0,0 +1,14 @@
+  private class arquero :(nombre, edad, raza, genero, gustos):
+  private string nombre=nombre;
+  private int edad= edad;
+  private string raza=raza;
+  private  string genero=genero;
+  private  string gustos= gustos;
+  // atributos constructor 
+   
+   private mago (){
+    private nombre=mago;
+    private edad=20;
+    private raza=mestizo;
+    private genero= masculino;
+    private gustos= correr;
